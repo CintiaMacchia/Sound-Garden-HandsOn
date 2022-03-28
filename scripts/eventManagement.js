1faecff78447a1b4be59a84119fd746e79816d99
Felipe Mendes <lfelipecmendes@icloud.com>
2022-03-28 13:59:28 -0300

    funcionalidades painel de controle

---
+++ scripts/eventManagement.js
@@ -0,0 +1,8 @@
+//Cadastrar novos eventos//
+
+const inputNome = document.querySelector('#nome');
+const inputAtracoes = document.querySelector('#atracoes');
+const inputDescricao = document.querySelector('#descricao');
+const inputData = document.querySelector('#data');
+const inputLotacao =document.querySelector('#lotacao');
+
