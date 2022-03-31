4a9991109a0e800ff5487576d806ca355cd3415f
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-03-30 21:08:18 -0300

    Subindo modal

--- scripts/eventCards.js
+++ scripts/eventCards.js
@@ -12,19 +12,19 @@ const listEvents = async () => {
   for (let index = 0; index < 3; index++) {
     const datas = responseContent[index].scheduled;
     const datasFormatadas = new Date(datas);
+    console.log(responseContent[index]._id);
     htmlData += ` <article class="evento card p-5 m-3">
         <h2>${responseContent[index].name} - ${datasFormatadas.toLocaleString(
       "en-GB"
     )}</h2>
         <h4>${responseContent[index].attractions}</h4>
         <p>${responseContent[index].description}</p>
-        <a href="#" event-id="${
-          responseContent[index]._id
-        }" class="btn btn-primary">reservar ingresso</a>
+        <a href="#?id=${responseContent[index]._id}" event-id="${
+      responseContent[index]._id
+    }" class="btn btn-primary">reservar ingresso</a>
     </article>`;
   }
   cardsContainer.innerHTML += htmlData;
 };
 
 listEvents();
-
