a311f0c464312d25847a0c65be842e5a0ceea7dc
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-03-30 21:06:31 -0300

    Subindo modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,19 +1,44 @@
-let modal = document.querySelector("#modal");
-const buttonOpenModal = document.querySelector(".btn-primary");
+const buttonOpenModal = document.querySelector("#cardContainer");
+console.log(buttonOpenModal);
 const buttonCloseModal = document.querySelector(".button-close-modal");
+const buttonBooking = document.querySelector(".button-booking");
+const inputEvento = document.querySelector("#evento");
+const inputDataEvento = document.querySelector("#data-evento");
+const inputNumberTickets = document.querySelector("#number-tickets");
+const inputNome = document.querySelector("#nome");
+const inputEmail = document.querySelector("#e-mail");
 
-
-
-buttonOpenModal.addEventListener("click", function(){ 
-    let modal = document.querySelector("#dv-modal");
-    modal.style.display = "block"
-    document.body.style.overflow = 'hidden';
+buttonOpenModal.addEventListener(
+  "click",
+  (openModal = () => {
+    document.querySelector("#dv-modal").style.display = "block";
+    // carregaModal();
   })
-
-  buttonCloseModal.addEventListener("click", function(){ 
-    let modal = document.querySelector("#dv-modal");
-    modal.style.display = 'none';
-    document.body.style.overflow = 'auto';
- })
+);
 
 
+buttonBooking.addEventListener("click", async (booking) => {
+  const newBooking = {
+    owner_name: inputNome.value,
+    owner_email: inputEmail.value,
+    number_tickets: inputNumberTickets.value,
+    event_id:"6244e45d44be9a585dfd9704"
+  }
+  const mountURL = {
+    method : 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newBooking),
+    redirect: 'follow'
+  }
+  const response = await fetch(`${BASE_URL}/bookings`, mountURL)
+  const responseContent = await response.json();
+  alert('Reserva Efetuada!'); 
+});
+
+buttonCloseModal.addEventListener(
+  "click",
+  (closeModal = () =>
+    (document.querySelector("#dv-modal").style.display = "none"))
+);
