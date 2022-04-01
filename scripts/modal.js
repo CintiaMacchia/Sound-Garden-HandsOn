ef2c617a5f225208ee6093996a90ec63e83a09c8
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-04-01 01:33:38 -0300

    Versionando tratamento de erros

--- scripts/modal.js
+++ scripts/modal.js
@@ -9,30 +9,24 @@ const inputQtdeLugares = document.querySelector("#lugares-disponiveis");
 const inputDataEvento = document.querySelector("#data-evento");
 let idURL = window.location.href.split("=")[1];
 
-
 function adicionarEvento() {
   buttonsOpenModal = document.querySelectorAll(".openModal");
-  buttonsOpenModal.forEach(btn => {
+  buttonsOpenModal.forEach((btn) => {
     btn.addEventListener("click", () => {
-        document.querySelector("#dv-modal").style.display = "block";
-        //console.log(btn);
-        //console.log(btn.getAttribute('event-id'));
-        listEventsById(btn.getAttribute('event-id'));
-      }
-    );
+      document.querySelector("#dv-modal").style.display = "block";
+      listEventsById(btn.getAttribute("event-id"));
+    });
   });
 }
 
 buttonBooking.addEventListener("click", async (booking) => {
   const idURL = window.location.href.split("=")[1];
-  console.log(idURL);
   const newBooking = {
     owner_name: inputNome.value,
     owner_email: inputEmail.value,
     number_tickets: inputNumberTickets.value,
     event_id: idURL,
   };
-  console.log(newBooking);
   const mountURL = {
     method: "POST",
     headers: {
@@ -41,12 +35,28 @@ buttonBooking.addEventListener("click", async (booking) => {
     body: JSON.stringify(newBooking),
     redirect: "follow",
   };
-  const response = await fetch(`${BASE_URL}/bookings`, mountURL);
-  const responseContent = await response.json();
-  alert("Reserva Efetuada!");
-  if (window.location.href.indexOf("index.html") > 0) {
-    return window.location.replace("./index.html");
-  } else return window.location.replace("./eventos.html");
+  try {
+    const response = await fetch(`${BASE_URL}/bookings`, mountURL);
+
+    if (response.status != 201) {
+      if (checaEmail(inputEmail.value) == "NOK") {
+        alert("Informe um e-mail válido!");
+      } else {
+        alert("Problema com os dados da reserva! Tente novamente");
+      }
+      returnBegin();
+    } else {
+      console.log(response.status);
+
+      const responseContent = await response.json();
+
+      alert("Reserva Efetuada!");
+      returnBegin();
+    }
+  } catch (error) {
+    alert("Erro tente reservar novamente!");
+    returnBegin();
+  }
 });
 
 buttonCloseModal.addEventListener(
@@ -59,10 +69,26 @@ const listEventsById = async (event_id) => {
   const request = await fetch(`${BASE_URL}/events/${event_id}`, requestOptions);
   const responseContent = await request.json();
   //console.log(responseContent);
-  const { name, number_tickets, scheduled } = responseContent
+  const { name, number_tickets, scheduled } = responseContent;
   const datas = scheduled;
   const datasFormatadas = new Date(datas).toLocaleString("pt-BR");
   inputEvento.value = name;
   inputDataEvento.value = datasFormatadas;
   inputQtdeLugares.value = number_tickets;
-}
+};
+
+//funções uteis
+returnBegin = () => {
+  if (window.location.href.indexOf("index.html") > 0) {
+    return window.location.replace("./index.html");
+  } else return window.location.replace("./eventos.html");
+};
+
+checaEmail = (campoEmail) => {
+  if (
+    campoEmail == "" ||
+    campoEmail.indexOf("@") == -1 ||
+    campoEmail.indexOf(".") == -1
+  )
+    return "NOK";
+};
